f1d752c6a806b03d7fee00155acc13c1d4f5645a
landofcoder <landofcoder@gmail.com>
2021-11-22 17:53:31 +0700

    added helper data

---
+++ Helper/Data.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Landofcoder
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the landofcoder.com license that is
+ * available through the world-wide-web at this URL:
+ * https://landofcoder.com/license
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category   Landofcoder
+ * @package    Lofmp_HelpDesk
+ * @copyright  Copyright (c) 2021 Landofcoder (https://landofcoder.com/)
+ * @license    https://landofcoder.com/LICENSE-1.0.html
+ */
+
+namespace Lofmp\HelpDesk\Helper;
+
+class Data extends \Lof\HelpDesk\Helper\Data
+{
+
+}
